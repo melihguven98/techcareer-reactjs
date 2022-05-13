f88958c6638f3e641c6598cf01da392c886c443c
Melih <melihguven98@outlook.com>
2022-05-13 19:16:53 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import Content from "./Content";
+import Footer from "./Footer";
+import Header from "./Header";
+import NavBar from "./NavBar";
+
+
+function App() {
+  return (
+    <>
+      <NavBar />      
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
